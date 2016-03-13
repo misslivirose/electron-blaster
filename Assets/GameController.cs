4bd06834dc96ab8adaf327944b11350ea9e285c5
Livi Erickson <Livi Erickson>
2016-03-13 13:12:42 -0700

    Sound and resets working

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using System.Collections;
+
 
 public class GameController : MonoBehaviour {
 
@@ -10,10 +12,16 @@ public class GameController : MonoBehaviour {
     public ParticleSystem _particles;
     public GameObject moleObject;
 
+    public GlobalAudioScript _globalSound;
+    public AudioClip _60SecondSound, _30SecondSound, _countdownSound, _321, _go, _laser, _hit;
+    public Material _sky;
+
     // Private variables - specified here
     float timeFromLastSpawn = 0.0f;
     float masterTimer = 90.0f;
     bool masterTimerRunning = false;
+    AudioSource _source;
+    float _skyRotation = 0.0f;
 
     // Alert remaining time variables
     bool _minuteMark = false;
@@ -23,20 +31,35 @@ public class GameController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+
+        _source = gameObject.GetComponent<AudioSource>();
+
+        // Run the countdown
         StartCoroutine(RunCountdown());
-         
+        ApplyDangerEffect();
 	}
 
     // Count Down from 3 to 0, then trigger game start
     IEnumerator RunCountdown()
     {
+       
+        _globalSound.SendMessage("PlaySound", _321);
+
         yield return new WaitForSeconds(1);
         _countdownText.text = "2";
+        _globalSound.SendMessage("PlaySound", _321);
+
         yield return new WaitForSeconds(1);
         _countdownText.text = "1";
+        _globalSound.SendMessage("PlaySound", _321);
+
+
         yield return new WaitForSeconds(1);
         _countdownText.fontSize = 10;
         _countdownText.text = "Go!";
+        _globalSound.SendMessage("PlaySound", _go);
+
+
         yield return new WaitForSeconds(1);
         _countdownText.enabled = false;
         BeginGame();
@@ -51,6 +74,7 @@ public class GameController : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
+
         if(!_finished)
         {
             timeFromLastSpawn += Time.deltaTime;
@@ -65,15 +89,23 @@ public class GameController : MonoBehaviour {
                 // 60 second mark
                 if (!_minuteMark & masterTimer <= 65 )
                 {
-                    Debug.Log("Minute warning");
+                    _globalSound.SendMessage("PlaySound", _60SecondSound);
                     _minuteMark = true;
                 }
                 // 30 second mark
                 else if (!_halfMinuteMark & masterTimer <= 35)
                 {
-                    Debug.Log("Half Minute Warning");
+                    _globalSound.SendMessage("PlaySound", _30SecondSound);
                     _halfMinuteMark = true;
                 }
+                // 10 second mark
+                else if(!_tenSecondCountDown & masterTimer <= 12)
+                {
+                    _globalSound.SendMessage("PlaySound", _countdownSound);
+                    _tenSecondCountDown = true;
+
+                }
+                // Game is finished
                 else if (!_finished & masterTimer <= 0)
                 {
                     _finished = true;
@@ -81,10 +113,10 @@ public class GameController : MonoBehaviour {
                     _countdownText.text = _score.ToString();
                     _countdownText.enabled = true;
                     Destroy(moleObject);
+                    StartCoroutine(ResetGame());
                 }
             }
         }
- 
 	}
 
     // Update Score
@@ -101,6 +133,8 @@ public class GameController : MonoBehaviour {
     IEnumerator PlayEffect()
     {
         _particles.Emit(1);
+        _source.clip = _laser;
+        _source.Play();
         yield return new WaitForSeconds(1);
         PositionMole();
 
@@ -110,6 +144,9 @@ public class GameController : MonoBehaviour {
     // Radius = 5
     void PositionMole()
     {
+        _source.clip = _hit;
+        _source.Play();
+
         float angle = Random.Range(0.0f, 2*Mathf.PI);
 
         float xPos = 6f * Mathf.Sin(angle);
@@ -120,5 +157,21 @@ public class GameController : MonoBehaviour {
         GameObject.FindGameObjectWithTag("mole").transform.LookAt(new Vector3(0.0f, 0.0f, 0.0f));
         timeFromLastSpawn = 0.0f;     
     }
+
+    // Wait for 10 seconds, then return to the home screen
+    IEnumerator ResetGame()
+    {
+        yield return new WaitForSeconds(10);
+        SceneManager.LoadScene(0);
+    }
+
+    // Flash material walls to red 
+    void ApplyDangerEffect()
+    {
+        
+        _sky.SetColor("_TintColor", Color.red);
+        Debug.Log(_sky.ToString());
+    }
+    
    
 }
---
+++ Assets/GlobalAudioScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalAudioScript : MonoBehaviour {
+
+    public AudioSource _globalSource;
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // Play Sound
+    public void PlaySound(AudioClip _soundToPlay)
+    {
+        _globalSource.clip = _soundToPlay;
+        _globalSource.Play();
+    }
+}
